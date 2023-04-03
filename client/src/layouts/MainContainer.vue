7658d6f96be03f0fe415c3dc6ad8b134a2d12b78
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-03 18:46:33 +0900

    Home init

--- client/src/layouts/MainContainer.vue
+++ client/src/layouts/MainContainer.vue
@@ -1,6 +1,6 @@
 <template>
-    <main>
-        <div class='w-full h-screen bg-blue-400'>
+    <main class='bg-gray-300'>
+        <div class='flex w-full min-h-screen h-full max-w-7xl mx-auto p-10'>
             <slot></slot>
         </div>
     </main>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,6 +4,21 @@ import MainContainer from '../layouts/MainContainer.vue'
 
 <template>
   <MainContainer>
-    <div>HI</div>
+    <div class='flex flex-row w-full h-fit my-auto space-x-10'>
+      <div class="basis-1/2 justify-center my-auto space-y-10 pr-10">
+        <div class='space-y-3'>
+          <h1 class='text-6xl font-bold'>Find useless stats!</h1>
+          <h3 class='text-lg'>Ever wonder how many points LeBron scored on his birthday? How about on Valentine's? NBooA
+            allows you to view player stats that you'll never need.</h3>
+        </div>
+        <button class='bg-blue-400 rounded-full p-2 text-sm text-white'>
+          View Players
+        </button>
+      </div>
+      <div class="basis-1/2 p-4">
+        <img
+          src="https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.cleveland.com/home/cleve-media/width2048/img/datacentral/photo/lebron-james-birthday-09192d395471f026.jpg" />
+      </div>
+    </div>
   </MainContainer>
 </template>
