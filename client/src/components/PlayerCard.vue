eaf2ee57cf3e03aa89236622db0db6539bb0e725
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-03 18:46:38 +0900

    Player view init

--- client/src/components/PlayerCard.vue
+++ client/src/components/PlayerCard.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import type { Player } from '../types'
-
+import { formatDate } from '../utils/date'
 defineProps<{
     player: Player
 }>()
 </script>
 
 <template>
-    <section>
-        <div>
-            <p>{{ player.first_name }}</p>
-        </div>
-    </section>
+    <div class='shadow-md bg-gray-50 p-4 rounded-lg'>
+        <img :src="player.image_url" />
+        <p>{{ player.first_name }} {{ player.last_name }}</p>
+        <p>{{ formatDate(player.birthday) }}</p>
+        <p>{{ player.start_year }} ~ {{ player.end_year || 'current' }}</p>
+    </div>
 </template>
