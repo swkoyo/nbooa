5d17c984db2fe0aefbb9376fa01f94486c1acb63
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-04 11:22:56 +0900

    Update padding

--- client/src/layouts/MainContainer.vue
+++ client/src/layouts/MainContainer.vue
@@ -5,7 +5,7 @@ import NavBar from '../components/NavBar.vue'
 <template>
     <main class='bg-gray-300'>
         <NavBar />
-        <div class='flex w-full min-h-screen h-full max-w-7xl mx-auto p-10'>
+        <div class='flex w-full min-h-screen h-full max-w-7xl mx-auto px-10 pt-24'>
             <slot></slot>
         </div>
     </main>
