77999f8f9fb66cac9a6966b03aba88da53625f23
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-04 14:26:17 +0900

    Navbar update

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -4,14 +4,16 @@ import router from '@/router';
 
 <template>
     <nav>
-        <div class='flex flex-row w-full dark:bg-slate-900 absolute top-0 p-4'>
-            <div @click="router.push('/')" class="inline-flex items-center space-x-2 hover:cursor-pointer">
-                <img class='h-10' src="../assets/logo.png" />
-                <h1 class='text-xl font-bold'>NBooA</h1>
+        <div class='w-full dark:bg-slate-900 absolute top-0 p-4'>
+            <div class='flex flex-row w-full max-w-7xl mx-auto'>
+                <div @click="router.push('/')" class="inline-flex items-center space-x-2 hover:cursor-pointer">
+                    <img class='h-10' src="../assets/logo.png" />
+                    <h1 class='text-xl font-bold'>NBooA</h1>
+                </div>
+                <div class='flex-1' />
+                <button @click="router.push('/players')"
+                    class='bg-indigo-500 rounded-lg p-2 text-md font-medium hover:bg-opacity-40'>Players</button>
             </div>
-            <div class='flex-1' />
-            <button @click="router.push('/players')"
-                class='bg-indigo-500 rounded-lg p-2 text-md font-medium hover:bg-opacity-40'>Players</button>
         </div>
     </nav>
 </template>
