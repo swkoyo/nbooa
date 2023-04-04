f14c2ccfc276f027a0c7adbff73a03caf5b28403
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-04 10:11:44 +0900

    Navbar init

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import router from '@/router';
+</script>
+
+<template>
+    <nav>
+        <div class='flex flex-row w-full bg-blue-400 absolute top-0 p-4'>
+            <div>NBooA</div>
+            <div class='flex-1' />
+            <button @click="router.push('/')" class='bg-blue-400 rounded-sm p-2'>Home</button>
+            <button @click="router.push('/players')" class='bg-blue-400 rounded-sm p-2'>Players</button>
+        </div>
+    </nav>
+</template>
--- client/src/layouts/MainContainer.vue
+++ client/src/layouts/MainContainer.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import NavBar from '../components/NavBar.vue'
+</script>
+
 <template>
     <main class='bg-gray-300'>
+        <NavBar />
         <div class='flex w-full min-h-screen h-full max-w-7xl mx-auto p-10'>
             <slot></slot>
         </div>
