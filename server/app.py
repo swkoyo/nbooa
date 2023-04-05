1e30d92421eb6cc86c63f620f655053bfc3c61cc
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-05 14:00:15 +0900

    remove create_app and create requirements.txt

--- server/app.py
+++ server/app.py
@@ -11,35 +11,33 @@ from db.client import db
 from db.seed.data import PLAYERS
 from schemas.client import ma
 
+app = Flask(__name__)
+CORS(app)
+app.config.from_object(Config)
+migrate = Migrate(app, db)
+db.init_app(app)
+ma.init_app(app)
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    CORS(app)
-    app.config.from_object(config_class)
-    migrate = Migrate(app, db)
-    db.init_app(app)
-    ma.init_app(app)
+app.register_blueprint(player_route)
 
-    app.register_blueprint(player_route)
 
-    @app.route(f"/{URL_PREFIX}/health")
-    def get_health():
-        return jsonify({"name": "NBooA API", "status": "OK", "version": VERSION})
+@app.route(f"/{URL_PREFIX}/health")
+def get_health():
+    return jsonify({"name": "NBooA API", "status": "OK", "version": VERSION})
 
-    @app.cli.command("seed")
-    def seed():
-        for player in PLAYERS:
-            m, d, y = player["birthday"].split("-")
-            new_player = Player(
-                first_name=player["first_name"],
-                last_name=player["last_name"],
-                birthday=datetime(int(y), int(m), int(d)),
-                image_url=player["image_url"],
-                _id=player["_id"],
-                start_year=player["start_year"],
-                end_year=player["end_year"],
-            )
-            db.session.add(new_player)
-            db.session.commit()
 
-    return app
+@app.cli.command("seed")
+def seed():
+    for player in PLAYERS:
+        m, d, y = player["birthday"].split("-")
+        new_player = Player(
+            first_name=player["first_name"],
+            last_name=player["last_name"],
+            birthday=datetime(int(y), int(m), int(d)),
+            image_url=player["image_url"],
+            _id=player["_id"],
+            start_year=player["start_year"],
+            end_year=player["end_year"],
+        )
+        db.session.add(new_player)
+        db.session.commit()
