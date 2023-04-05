584142afe28dc0701b671b53002486fe3532d046
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-05 14:43:36 +0900

    update env

--- server/config.py
+++ server/config.py
@@ -10,5 +10,6 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    ENV = "development" if os.environ.get("FLASK_DEBUG") == "1" else "production"
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'db', 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
